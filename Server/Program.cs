b5c0d9e26cc7643c39aab72fc6381413d877935c
Sh0w3D <49204446+Sh0w3D@users.noreply.github.com>
2022-03-25 22:49:00 +0100

    Add needed files and usings

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Server;
 using Server.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+var seed = args.Contains("/seed");
+if(seed)
+{
+    args = args.Except(new[] { "/seed" }).ToArray();
+}
 
+var builder = WebApplication.CreateBuilder(args);
+var defauldConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (seed)
+{
+    SeedData.EnsureSeedData(defauldConnectionString);
+}
 
 var assembly = typeof(Program).Assembly.GetName().Name;
-var defauldConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 
 builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
     options.UseNpgsql(defauldConnectionString,
@@ -30,5 +42,13 @@ builder.Services.AddIdentityServer()
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
 app.UseIdentityServer();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 app.Run();
