32a9c6d2d40b6d36f9c328fa22163f644f55b6d0
Sh0w3D <49204446+Sh0w3D@users.noreply.github.com>
2022-03-25 21:41:09 +0100

    Add Identity and migrations

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defauldConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddIdentityServer()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseNpgsql(defauldConnectionString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseNpgsql(defauldConnectionString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseIdentityServer();
 app.Run();
