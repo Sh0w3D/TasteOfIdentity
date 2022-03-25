94d1b03ec6e9c88f64557e5df85e8c1ffbff2faf
Sh0w3D <49204446+Sh0w3D@users.noreply.github.com>
2022-03-25 19:16:09 +0100

    Add DbContext, migrations, Entities

---
+++ DataAccess/Entities/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    [Table("Student", Schema = "public")]
+    public class Student
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString("D");
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime UpdatedTime { get; set; } = DateTime.UtcNow;
+
+
+
+    }
+}
--- TaseOfIdentity/Program.cs
+++ TaseOfIdentity/Program.cs
@@ -1,6 +1,14 @@
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var dbConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(dbConnectionString));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
